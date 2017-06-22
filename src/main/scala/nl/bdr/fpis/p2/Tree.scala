0728d8c80e0bcd7207d009a40770e2eb6a0670a5
Bertjan Broeksema <bertjan.broeksema@bigdatarepublic.nl>
2017-06-22 21:22:54 +0200

    Add Tree exercises

--- src/main/scala/nl/bdr/fpis/p2/Tree.scala
+++ src/main/scala/nl/bdr/fpis/p2/Tree.scala
@@ -5,5 +5,22 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
+  // Exercise 3.25
+  //
+  // Write a function size that counts the number of nodes (leaves and branches) in a tree.
 
+  // Exercise 3.26
+  //
+  // Write a function maximum that returns the maximum element in a Tree[Int]. (Note: In Scala, you
+  // can use x.max(y) or x max y to compute the maximum of two integers x and y.)
+
+  // Exercise 3.27
+  //
+  // Write a function depth that returns the maximum path length from the root of a tree to any leaf.
+
+  // Exercise 3.28
+  //
+  // Write a function map, analogous to the method of the same name on List, that modifies each
+  // element in a tree with a given function.
+  def map[A, B](t: Tree[A])(f: A => B) = ???
 }

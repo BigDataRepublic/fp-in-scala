7633de4b561bfa6ce25ea72c3ae070032ef15567
Bertjan Broeksema <bertjan.broeksema@bigdatarepublic.nl>
2018-02-09 16:08:03 +0100

    Add implementations of Left and Right... for the Lazy

--- src/main/scala/nl/bdr/fpis/p3/Either.scala
+++ src/main/scala/nl/bdr/fpis/p3/Either.scala
@@ -20,6 +20,9 @@ trait Either[+E, +A] {
   def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
 }
 
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
+
 object Either {
   // Exercise - 4.7
   //
